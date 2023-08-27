61e082ba8867d621b12437978310e6e3163a45a2
Milan Mrljes <mrljes00@gmail.com>
2023-08-27 09:32:14 +0200

    TC-3 : Giving selectors to all tests

--- tests/selectors/common.ts
+++ tests/selectors/common.ts
@@ -16,5 +16,21 @@ export const selectors={
     "facebookButton" : '//*[@id="page_wrapper"]/footer/ul/li[2]/a',
     "linkedinButton" : '//*[@id="page_wrapper"]/footer/ul/li[3]/a',
     "footerText" : '© ',
-    "logoutBtn" : '//*[@id="logout_sidebar_link"]'
+    "logoutBtn" : '//*[@id="logout_sidebar_link"]',
+    "lockedOutUsername" : 'locked_out_user',
+    "lockedOutUserErrorMessage" : '//*[@id="login_button_container"]/div/form/div[3]/h3',
+    "problemUserName" : 'problem_user',
+    "itemImage" : '//*[@id="item_4_img_link"]',
+    "glitchUserName" : 'performance_glitch_user',
+    "wrongPassword" : 'test123',
+    "wrongPasswordErrorMessage" : '//*[@id="login_button_container"]/div/form/div[3]/h3',
+    "errorMessageTextForLockedUser" : 'Epic sadface: Sorry, this user has been locked out.',
+    "errorMessageTextForWrongUserNameAndPassowrd" : 'Epic sadface: Username and password do not match any user in this service',
+    "closeHamburgerButton" : '//*[@id="react-burger-cross-btn"]',
+    "allItemsButton" : '//*[@id="inventory_sidebar_link"]',
+    "aboutButton" : '//*[@id="about_sidebar_link"]',
+    "textFromAnotherPage" : '//*[@id="__next"]/div[2]/div[1]/div/div[1]/div[1]/div/div[4]/div[1]/a/button',
+    "resetAppBtn" : '//*[@id="reset_sidebar_link"]',
+
+
 }
